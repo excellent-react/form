6c75ecda911cfc5c96ecac6fa31d7d5b73e65ae7
panchaldeep009 <panchaldeep009@gmail.com>
2020-06-25 00:04:34 -0400

    Proves concept of capturing values from form input

--- libs/form/src/lib/Form.spec.tsx
+++ libs/form/src/lib/Form.spec.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 
 import Form from './Form';
 
@@ -8,4 +9,23 @@ describe(' Form', () => {
     const { baseElement } = render(<Form />);
     expect(baseElement).toBeTruthy();
   });
+
+  it('should return changed Input', async () => {
+    const onSubmitMocked = jest.fn();
+    const form = render(
+      <Form onSubmit={onSubmitMocked}>
+        <input name="a-input" type="text" aria-label="a-input" />
+        <button type="submit" aria-label="submit">submit</button>
+      </Form>
+    );
+    const input = form.getByLabelText('a-input');
+    const inputText = "a input text";
+    const button = form.getByLabelText('submit');
+
+    await userEvent.type(input, inputText);
+    userEvent.click(button);
+    
+    expect(onSubmitMocked).toBeCalledWith({ 'a-input': inputText });
+  });
+
 });
--- libs/form/src/lib/Form.tsx
+++ libs/form/src/lib/Form.tsx
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 
-import styled from '@emotion/styled';
+export type FormInputDataType = string | number | boolean | File | FormInputDataType[];
+export type FormData<K extends string> = Record<K, FormInputDataType>;
 
-/* eslint-disable-next-line */
-export interface FormProps {}
 
-const StyledForm = styled.div`
-  color: pink;
-`;
+export interface FormProps<F extends FormData<string>> extends Omit<HTMLFormElement, 'onSubmit' | 'onChange'> {
+  onSubmit?: (data: F) => void;
+}
 
-export const Form = (props: FormProps) => {
+export const Form = <F extends FormData<string>>(props: FormProps<F>) => {
+  const { children, onSubmit, ...formProps } = props;
+  const [formData, setFormData] = useState({  } as F);
+
+  const handleChange = useCallback((e: React.ChangeEvent<HTMLFormElement>) => {
+    const inputTarge = e.target;
+    if (inputTarge && inputTarge?.name && inputTarge?.value) {
+      setFormData(currentFormData => ({...currentFormData, [inputTarge.name] : inputTarge.value }));
+    }
+  }, [setFormData]);
+
+  const handleSubmit = useCallback((e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onSubmit(formData);
+  }, [onSubmit, formData]);
+
   return (
-    <StyledForm>
-      <h1>Welcome to form!</h1>
-    </StyledForm>
+    <form onSubmit={handleSubmit} onChange={handleChange} {...formProps}>
+      {children}
+    </form>
   );
 };
 
