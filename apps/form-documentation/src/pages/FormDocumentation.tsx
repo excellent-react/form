e5687f99cfce28d2a2da0bc9d5914ba68bc02469
Deep Panchal <39134481+panchaldeep009@users.noreply.github.com>
2020-10-09 13:42:30 -0400

    Documentation loads for readme (#12)

--- apps/form-documentation/src/pages/FormDocumentation.tsx
+++ apps/form-documentation/src/pages/FormDocumentation.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import Markdown from 'markdown-to-jsx';
+
+const fetchDocumentationMarkdown = () => new Promise<string>((resolver, reject) => {
+  fetch('https://raw.githubusercontent.com/panchaldeep009/form/master/README.md')
+    .then(res => res.text())
+    .then(resolver)
+    .catch(reject);
+});
 
 const FormDocumentation: React.FC = () => {
-  return (
-    <div>
-      <a href="https://github.com/excellent-react/form">Excellent React Form Documentation</a>
-    </div>
-  )
+  const [markdownData, setMarkdownData] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      const markdownData = await fetchDocumentationMarkdown();
+      setMarkdownData(markdownData);
+      setLoading(false);
+    })();
+  }, []);
+
+  return loading ? (
+    <div> Loading... </div>
+  ) : (
+    <Markdown 
+      children={markdownData}
+      options={{
+        disableParsingRawHTML: true
+      }} />
+  );
 }
 
 export default FormDocumentation
