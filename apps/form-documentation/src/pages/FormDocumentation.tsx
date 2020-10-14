f200e966a6d3b4d8f8444959a04aaded55e8ef8e
Italo Di Renzo <italodirenzo93@gmail.com>
2020-10-14 09:20:05 -0400

    Add styles and logos to docs (#17)
    * Add Chakra UI
    
    * Add loading indicator
    
    * Add centred container
    
    * Change HTML img syntax to markdown in README.md
    
    * Add extra styles to markdown

--- apps/form-documentation/src/pages/FormDocumentation.tsx
+++ apps/form-documentation/src/pages/FormDocumentation.tsx
@@ -1,12 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import Markdown from 'markdown-to-jsx';
+import {
+  Box,
+  BoxProps,
+  Code,
+  Flex,
+  Heading,
+  HeadingProps,
+  Image,
+  List,
+  Spinner,
+  Text,
+} from '@chakra-ui/core';
 
-const fetchDocumentationMarkdown = () => new Promise<string>((resolver, reject) => {
-  fetch('https://raw.githubusercontent.com/panchaldeep009/form/master/README.md')
-    .then(res => res.text())
-    .then(resolver)
-    .catch(reject);
-});
+const fetchDocumentationMarkdown = () =>
+  new Promise<string>((resolver, reject) => {
+    fetch(
+      'https://raw.githubusercontent.com/panchaldeep009/form/master/README.md'
+    )
+      .then((res) => res.text())
+      .then(resolver)
+      .catch(reject);
+  });
 
 const FormDocumentation: React.FC = () => {
   const [markdownData, setMarkdownData] = useState('');
@@ -22,14 +37,46 @@ const FormDocumentation: React.FC = () => {
   }, []);
 
   return loading ? (
-    <div> Loading... </div>
+    <Flex
+      flexDir="column"
+      justifyContent="center"
+      alignItems="center"
+      height="100vh"
+    >
+      <Spinner
+        thickness="4px"
+        speed="0.65s"
+        emptyColor="gray.200"
+        color="blue.500"
+        size="xl"
+        aria-busy="true"
+      />
+    </Flex>
   ) : (
-    <Markdown 
-      children={markdownData}
-      options={{
-        disableParsingRawHTML: true
-      }} />
+    <Box margin="auto" width={['100%', '80%', '60%', '50%']}>
+      <Markdown
+        children={markdownData}
+        options={{
+          disableParsingRawHTML: true,
+          overrides: {
+            h1: {
+              component: Heading,
+              props: { size: 'xl', my: 6 } as HeadingProps,
+            },
+            h2: {
+              component: Heading,
+              props: { size: 'lg', my: 4 } as HeadingProps,
+            },
+            p: { component: Text, props: { my: 2 } as BoxProps },
+            img: { component: Image },
+            ul: { component: List, props: { styleType: 'disc' } },
+            code: { component: Code },
+            pre: { component: Code, props: { as: 'pre', p: 4 } as BoxProps },
+          },
+        }}
+      />
+    </Box>
   );
-}
+};
 
-export default FormDocumentation
+export default FormDocumentation;
